12f85a073959bc39972285efe514edbb1174bbdc
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-12-30 15:51:54 +0100

    Playing with react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <h1>This is page one</h1>;
+};
+
+const PageTwo = () => {
+  return <h1>This is page Two</h1>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pageTwo" component={PageTwo} />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
