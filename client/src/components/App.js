debe062ba9680feca1762aed01079ce70a3d637d
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-12-30 16:23:12 +0100

    Add all Basic components with basic routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const PageOne = () => {
-  return <h1>This is page one</h1>;
-};
-
-const PageTwo = () => {
-  return <h1>This is page Two</h1>;
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pageTwo" component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
       </div>
     </Router>
   );
