d8f1052ab35f6382828e54ccc6c7acdcbca75060
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-12-31 23:50:24 +0100

    Create A stream Using json-server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,9 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onFormSubmit(formValues) {
-    console.log(formValues);
-  }
+  onFormSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -61,7 +63,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapper = reduxForm({
   form: 'streamCreate',
   validate: validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapper);
