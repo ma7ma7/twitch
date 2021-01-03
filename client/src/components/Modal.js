93fcb7c337366a3e019cb0cf84055c7a15301290
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-03 22:46:03 +0100

    Implement profiler for Modal And delete Stream

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Modal = () => {
+const Modal = (props) => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
       <div className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Do you want delete Stream</div>
-        <div className="actions">
-          <button className="ui button danger">Delete</button>
-          <button className="ui button primary">Cancel</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
